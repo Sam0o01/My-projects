4a0faba92a4069247f9c0115fec20665485335ba
Sam0o01 <olododosmauel17@gmail.com>
2024-05-31 14:39:27 +0100

    My-projects

---
+++ Age calc/Main.py
@@ -0,0 +1,6 @@
+fName = input("Enter your first name: ")
+lName = input("Enter your last name: ")
+Age = int(input("Enter your age: "))
+New_age = Age + 10
+print("My name is", fName, lName, "and i'm" ,Age )
+print(fName, " will be ", New_age, "In the next 10 years" )
---
+++ Dqol/deep.py
@@ -0,0 +1,5 @@
+Answer = input("What is the answer to the Great Question of life Thou Feeble Human : ")
+if Answer == "42" or Answer == "forty-two" or Answer == "forty two" :
+    print("Yes")
+else :
+    print("No, clown")
